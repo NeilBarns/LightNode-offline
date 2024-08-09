b1d0968351085682ce11502d8bcef728c591adfd
djo <michaelsbarnedo@gmail.com>
2024-08-10 07:15:51 +0800

    Create EEPROMSPIFFSReset.ino

---
+++ EEPROMSPIFFSReset.ino
@@ -0,0 +1,30 @@
+#include <EEPROM.h>
+#include <FS.h>
+
+void setup() {
+  Serial.begin(115200);
+
+  // Initialize EEPROM with a size of 512 bytes
+  EEPROM.begin(512);
+
+  // Clear EEPROM by writing zeros
+  for (int i = 0; i < 512; i++) {
+    EEPROM.write(i, 0);
+  }
+
+  // Commit the changes to EEPROM
+  EEPROM.commit();
+if (!SPIFFS.begin()) {
+    Serial.println(F("Failed to mount file system"));
+    return;
+  }
+
+	SPIFFS.format();
+SPIFFS.end();
+
+  Serial.println("EEPROM cleared.");
+}
+
+void loop() {
+  // Nothing to do here
+}
